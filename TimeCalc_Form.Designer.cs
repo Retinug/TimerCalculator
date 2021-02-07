f03e423cc85b03c75af7cddf2f87f21ee4aa90be
Retinug <retinug@gmail.com>
2021-02-07 16:26:47 +0300

    Init

---
+++ TimeCalc_Form.Designer.cs
@@ -0,0 +1,51 @@
+﻿
+namespace TimerCalculator
+{
+    partial class TimCalc_Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TimCalc_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "TimCalc_Form";
+            this.Text = "Timer calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ TimeCalc_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerCalculator
+{
+    public partial class TimCalc_Form : Form
+    {
+        public TimCalc_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
